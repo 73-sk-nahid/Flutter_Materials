b0bd29b78cfc853b6fc28a981397d32f155d9a0f
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-10-16 23:47:47 +0600

    new class added

--- lib/animated_gradient.dart
+++ lib/animated_gradient.dart
@@ -3,18 +3,34 @@ import 'package:flutter/services.dart';
 import 'package:flutter_materials/app_theme.dart';
 import 'package:flutter_materials/theme_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends HookConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  //State<MyApp> createState() => _MyAppState();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    final appThemeState = ref.watch(appThemeStateNotifier);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: appThemeState.isDarkModeEnabled ? ThemeMode.dark : ThemeMode.light,
+      //_MyAppState(),
+    );
+  }
 }
+
+
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
@@ -88,7 +104,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       //themeMode: appThemeState.isDarkMdeEnabled ? ThemeData.dark: ThemeMode.light,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "Flutter Material",
             style: TextStyle(
               color: Colors.black,
@@ -121,118 +137,3 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   }
 }
 
-/*
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Alignment> _topAlignmentAnimation;
-  late Animation<Alignment> _bottomAlignmentAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 4));
-    _topAlignmentAnimation = TweenSequence<Alignment>(
-      [
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topLeft, end: Alignment.topRight),
-          weight: 1,
-        ),
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topRight, end: Alignment.bottomRight),
-          weight: 1,
-        ),
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomRight, end: Alignment.bottomLeft),
-          weight: 1,
-        ),
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomLeft, end: Alignment.topLeft),
-          weight: 1,
-        ),
-      ],
-    ).animate(_controller);
-
-    _bottomAlignmentAnimation = TweenSequence<Alignment>(
-      [
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomRight, end: Alignment.bottomLeft),
-          weight: 1,
-        ),
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomLeft, end: Alignment.topLeft),
-          weight: 1,
-        ),
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topLeft, end: Alignment.topRight),
-          weight: 1,
-        ),
-        TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topRight, end: Alignment.bottomRight),
-          weight: 1,
-        ),
-      ],
-    ).animate(_controller);
-
-    _controller.repeat();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Materials"),
-        ),
-        body: Center(
-          child: AnimatedBuilder(
-              animation: _controller,
-              builder: (context, _) {
-                return Container(
-                  width: 300,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: const [
-                        Color(0xffF99E43),
-                        Color(0xFFDA2323),
-                      ],
-                      begin: _topAlignmentAnimation.value,
-                      end: _bottomAlignmentAnimation.value,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                );
-              }),
-        ),
-      ),
-    );
-  }
-}
- */
